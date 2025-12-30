5d18d15caccb4346d6f5c5552c4ec415890b56a3
Syarifudin <suicideudin@gmail.com>
2025-12-30 16:54:43 +0700

    notification versi vanilla js

--- resources/views/partials/DashboardNavigation.blade.php
+++ resources/views/partials/DashboardNavigation.blade.php
@@ -26,7 +26,59 @@
                 </form>
                 <!-- end::Search input -->
 
-                <livewire:notifikasi/>
+              
+                <!-- start::Notifications -->
+                <div 
+                x-data="{ linkActive: false }"
+                    class="relative mx-6"
+                >
+                    <!-- start::Main link -->
+                    <div
+                        @click="linkActive = !linkActive"
+                        class="cursor-pointer flex"
+                    >
+                        <svg class="w-6 h-6 cursor-pointer hover:text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
+                        <sub>
+                            <span id="notif-badge-luar-1" class="bg-red-600 text-gray-100 px-1.5 py-0.5 rounded-full -ml-1">0</span>
+                        </sub>
+                    </div>
+                    <!-- end::Main link -->
+                    
+                    <!-- start::Submenu -->
+                    <div 
+                        x-show="linkActive"
+                        @click.away="linkActive = false"
+                        x-cloak
+                        class="absolute right-0 w-96 top-11 border border-gray-300 z-10"
+                    >
+                    <div>
+                        <!-- start::Submenu content -->
+                        <div class="bg-white rounded max-h-96 overflow-y-scroll custom-scrollbar">
+                            <!-- start::Submenu header -->
+                            <div class="flex items-center justify-between px-4 py-2">
+                                    <span class="font-bold">Notifications</span>
+    
+                                    <span  id="notif-badge-luar-2" class="bg-red-600 text-gray-100 px-1.5 py-0.5 rounded-full -ml-1">0</span>
+                            </div>
+                            <hr>
+                            <!-- end::Submenu header -->
+
+                            <!-- start::Submenu link -->
+                            <div id="notif-list">
+
+                            </div>
+                            <center id="no-notif-message" >
+                                <p class="text-muted mt-2 p-4 text-sm text-gray-400">Tidak ada notifikasi baru</p>
+                            </center>
+                            <!-- end::Submenu link -->
+                        </div>
+                        <!-- end::Submenu content -->
+                    </div>
+                    
+                    </div>
+                    <!-- end::Submenu -->
+                </div>
+                <!-- end::Notifications -->
                 
                 <!-- start::Profile -->
                 <div 
